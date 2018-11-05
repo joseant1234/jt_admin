9ab67dc2594849142eaed12ddbf13543254ed91c
Jose Tafur <j-osea-nt@hotmail.com>
2018-11-04 21:07:54 -0500

    refactoring

--- app/helpers/jt_admin/application_helper.rb
+++ app/helpers/jt_admin/application_helper.rb
@@ -2,13 +2,8 @@ module JtAdmin
   module ApplicationHelper
 
     def li_paint_slider(text, path, controllers)
-      if controllers.include? controller.controller_name
-        link_css_class = 'white custom-color-text'
-      else
-        link_css_class = 'white-text'
-      end
-
-      link_to text, path, class: "collection-item option-item white-text #{link_css_class}"
+      link_css_class = controllers.include?(controller.controller_name) ? 'jt-secondary-color jt-main-color-text' : 'jt-secondary-color-text'
+      link_to text, path, class: "collection-item option-item #{link_css_class}"
     end
 
   end
